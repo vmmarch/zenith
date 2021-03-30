4d25ef1c29dbd42a9825924acc004bb50b95dcbe
luots <2968640488@qq.com>
2021-03-30 20:25:44 +0800

    test

--- viewport/child-window.cpp
+++ viewport/child-window.cpp
@@ -69,7 +69,7 @@ void child_window::Display()
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        // Update and Render additional Platform Windows
+        // Update and Render additional Platform Windows.
         // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
         //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
