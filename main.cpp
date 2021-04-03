c2c094fd189da7709657cc8742423c0d2396d135
luots <2968640488@qq.com>
2021-04-03 13:56:19 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
     __ZENITH_ERROR__("ERROR - Init error for {}", "Zenith3D");
 
 //    spdlog::info("Welcome to spdlog!");
-//    spdlog::error("Some error message with arg: {}", 1);
+    spdlog::error("Some error message with arg: {}", 1);
 //
 //    spdlog::warn("Easy padding in numbers like {:08d}", 12);
 //    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
