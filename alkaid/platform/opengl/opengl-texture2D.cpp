9bec12f8b0d69d6ed183142cb5948ade29e8e008
215046148@qq.com <215046148@qq.com>
2021-04-15 20:40:17 +0800

    Add texture2D for opengl

---
+++ alkaid/platform/opengl/opengl-texture2D.cpp
@@ -0,0 +1,24 @@
+#include "opengl-texture2D.h"
+
+namespace alkaid
+{
+	void OpenGLTexture2D::read_image(v_cc path)
+	{
+	}
+	
+	void OpenGLTexture2D::bind()
+	{
+	}
+	
+	void OpenGLTexture2D::unbind()
+	{
+	}
+	
+	void OpenGLTexture2D::set_mod(surrmod mod, int coor, float* color)
+	{
+	}
+	
+	void OpenGLTexture2D::set_filter(int zoom, 	filtermod filter)
+	{
+	}
+}
---
+++ alkaid/platform/opengl/opengl-texture2D.h
@@ -0,0 +1,9 @@
+#include "render/texture/texture.h"
+
+namespace alkaid
+{
+	class OpenGLTexture2D : public Texture2D
+	{
+		
+	}
+}
