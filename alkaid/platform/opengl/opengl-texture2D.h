af859b699ad55c2150200dfe0f39ac9e8204c702
215046148@qq.com <215046148@qq.com>
2021-04-15 20:41:44 +0800

    Update

--- alkaid/platform/opengl/opengl-texture2D.h
+++ alkaid/platform/opengl/opengl-texture2D.h
@@ -1,9 +1,15 @@
+/**/
 #include "render/texture/texture.h"
 
 namespace alkaid
 {
 	class OpenGLTexture2D : public Texture2D
 	{
-		
+	public:
+		void read_image(v_cc) override;
+		void bind() override;
+		void unbind() override;
+		void set_mod(surrmod, int, float* = NULL) override;
+		void set_filter(int, filtermod) override;
 	}
 }
