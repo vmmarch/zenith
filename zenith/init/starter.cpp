178eedfe5ed4ba3e1758892381035e634f2da1dc
luots <2968640488@qq.com>
2021-04-21 10:55:32 +0800

    三角形

--- zenith/init/starter.cpp
+++ zenith/init/starter.cpp
@@ -78,6 +78,32 @@ namespace zenith
         float last_frame_time = 0.0f;
         int render_count = 0;
 
+        // ----------------------------------------
+        // 画三角形
+        unsigned int verbuf, verarr, verindex;
+
+        glGenVertexArrays(1, &verarr);
+        glBindVertexArray(verarr);
+
+        glGenBuffers(1, &verbuf);
+        glBindBuffer(GL_ARRAY_BUFFER, verbuf);
+
+        glGenBuffers(1, &verindex);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, verindex);
+
+        float vertices[] = {
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f, 0.5f, 0.0f,
+        };
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        unsigned int indices[] = { 0, 1, 2 };
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+
         while (running)
         {
             float time = (float) glfwGetTime();
@@ -85,6 +111,11 @@ namespace zenith
             last_frame_time = timestep;
 
             renderer->clear();
+
+            // ----------------------------------------------------
+            // GL render from there.
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
             layer_stack.update();
             imlayer->begin();
